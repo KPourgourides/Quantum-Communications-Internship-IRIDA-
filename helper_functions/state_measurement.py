1ba509ee346664069374b982ad568fb76ed570c1
Konstantinos Pourgourides <kotsiosp2001@gmail.com>
2026-07-04 13:48:50 +0000

    added argument and return types for the functions

--- helper_functions/state_measurement.py
+++ helper_functions/state_measurement.py
@@ -3,22 +3,21 @@ import strawberryfields as sf
 from strawberryfields.ops import *
 
 
-def measure_fock(n):
-    prog = sf.Program(1)
+def measure_fock(n:int) -> int:
 
+    prog = sf.Program(1)
     with prog.context as q:
         Fock(n) | q[0]
         MeasureFock() | q[0]
 
     #run the engine and get the state
     eng = sf.Engine("fock", backend_options={"cutoff_dim": 100})
-
     result = eng.run(prog)
 
-    return result.samples
+    return result.samples[0, 0]
 
 
-def measure_coherent(alpha, homodyne_angle):
+def measure_coherent(alpha:complex, homodyne_angle:float) -> float:
 
     prog = sf.Program(1)
     r = np.abs(alpha)
@@ -32,11 +31,11 @@ def measure_coherent(alpha, homodyne_angle):
     eng = sf.Engine("gaussian")
     result = eng.run(prog)
 
-    return result.samples
+    return result.samples[0, 0]
 
-def measure_squeezed(r, phi, homodyne_angle):
+def measure_squeezed(r:float, phi:float, homodyne_angle:float) -> float:
+
     prog = sf.Program(1)
-
     with prog.context as q:
         Vac | q[0]
         Sgate(r, phi) | q[0] 
@@ -47,24 +46,22 @@ def measure_squeezed(r, phi, homodyne_angle):
 
     result = eng.run(prog)
 
-    return result.samples
+    return result.samples[0, 0]
 
-def measure_displaced_squeezed(s_r, s_phi, alpha, homodyne_angle):
+def measure_displaced_squeezed(r_s:float, phi_s:float, alpha:complex, homodyne_angle:float):
+
     prog = sf.Program(1)
-
-    d_r = np.abs(alpha)
-    d_phi = np.angle(alpha)
+    r_d = np.abs(alpha)
+    phi_d = np.angle(alpha)
 
     with prog.context as q:
         Vac | q[0]
-        Sgate(s_r, s_phi) | q[0] 
-        Dgate(d_r, d_phi) | q[0]
+        Sgate(r_s, phi_s) | q[0] 
+        Dgate(r_d, phi_d) | q[0]
         MeasureHomodyne(homodyne_angle) | q[0]
-
-
 
     #run the engine and get the state
     eng = sf.Engine("gaussian")
     result = eng.run(prog)
 
-    return result.samples
+    return result.samples[0, 0]
