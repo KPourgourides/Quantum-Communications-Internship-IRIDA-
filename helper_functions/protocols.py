653fdfbc05c88e6dd31a68c619fd8db0b684af45
KMitsidi <kipmitsidiles@gmail.com>
2026-07-06 14:49:53 +0000

    Created protocol for squeezed displaced functions

--- helper_functions/protocols.py
+++ helper_functions/protocols.py
@@ -1,8 +1,9 @@
 import strawberryfields as sf
 from strawberryfields.ops import *
 import numpy as np
+import math
 from scipy.special import erfc
-from helper_functions.state_measurement import measure_coherent
+from helper_functions.state_measurement import *
 
 def perr_cs(alpha_grid:np.array, homodyne_angle:float, num_samples:int):
 
@@ -31,3 +32,31 @@ def perr_cs(alpha_grid:np.array, homodyne_angle:float, num_samples:int):
     N = np.abs(alpha_grid)**2
 
     return N, p_err
+
+
+def perr_dss(N_grid:np.array, beta_grid:np.array, homodyne_angle:float, num_samples:int):
+
+    #Calculation of error probability
+    #============================================
+    p_err = np.zeros((len(N_grid), len(beta_grid)))
+    
+    for i,N in enumerate(N_grid):
+        print(f"\rProgress: {i+1}/{len(N_grid)}", end="", flush=True)
+        alpha_grid = np.sqrt(N*(1-beta_grid)) 
+        
+        for k, beta in enumerate(beta_grid):
+            wrong_sign_counter = 0
+            r_s = math.asinh(np.sqrt(N*beta))
+            
+            for j in range(num_samples):    
+                coherent_sign = np.random.choice([1, -1])
+                
+                result = measure_displaced_squeezed(r_s, 0, coherent_sign*alpha_grid[k], homodyne_angle)
+                result_sign = np.sign(result)
+    
+                if (result_sign>=0 and coherent_sign<0) or (result_sign<0 and coherent_sign>0):
+                        wrong_sign_counter+= 1
+    
+            p_err[i][k] = wrong_sign_counter/num_samples
+
+    return p_err
