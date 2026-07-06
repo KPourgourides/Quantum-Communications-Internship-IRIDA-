da4deb4b691a07920c16f5de6fda0ebd380251b2
KMitsidi <kipmitsidiles@gmail.com>
2026-07-06 16:59:26 +0000

    plotted theoretical perr and helstrom bound

--- helper_functions/protocols.py
+++ helper_functions/protocols.py
@@ -4,6 +4,8 @@ import numpy as np
 import math
 from scipy.special import erfc
 from helper_functions.state_measurement import *
+from scipy.special import erfc
+
 
 def perr_cs(alpha_grid:np.array, homodyne_angle:float, num_samples:int):
 
@@ -60,3 +62,27 @@ def perr_dss(N_grid:np.array, beta_grid:np.array, homodyne_angle:float, num_samp
             p_err[i][k] = wrong_sign_counter/num_samples
 
     return p_err
+
+
+def P_err_theory(N, beta):
+    """
+    Theoretical homodyne error probability for DSS states.
+
+    Parameters
+    ----------
+    N : float or np.ndarray
+        Total photon number
+    beta : float or np.ndarray
+        Squeezing fraction (0 <= beta <= 1)
+
+    Returns
+    -------
+    P_err : float or np.ndarray
+    """
+
+    alpha = np.sqrt(N * (1 - beta))
+    Sigma = 1.0 / (N * beta + np.sqrt(1 + N * beta))
+
+    argument = (2 * alpha) / (np.sqrt(2) * Sigma)
+
+    return 0.5 * erfc(argument)
