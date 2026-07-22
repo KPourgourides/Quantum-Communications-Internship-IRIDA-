2f365254402e4353989d2aa9aa016a295c9d235b
KMitsidi <kipmitsidiles@gmail.com>
2026-07-22 14:51:18 +0300

    created files for impurity investigation

---
+++ protocol_impure.py
@@ -0,0 +1,11 @@
+import strawberryfields as sf
+from strawberryfields.ops import *
+import numpy as np
+import math
+from scipy.optimize import curve_fit, brentq
+from helper_functions.state_measurement import *
+from scipy.special import erfc
+import plotly.graph_objects as go
+from numpy.polynomial.hermite import hermgauss
+import matplotlib.pyplot as plt
+from scipy.optimize import brentq
