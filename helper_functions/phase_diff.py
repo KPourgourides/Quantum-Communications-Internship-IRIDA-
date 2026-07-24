f5ede800e7f6d32042ad2263aa1d7fa241c48168
KPourgourides <kotsiosp2001@gmail.com>
2026-07-24 20:30:37 +0300

    updated several functions

--- helper_functions/phase_diff.py
+++ helper_functions/phase_diff.py
@@ -1,17 +1,18 @@
 import strawberryfields as sf
 from strawberryfields.ops import *
 import numpy as np
-import math
-from scipy.optimize import curve_fit, brentq
-from helper_functions.state_measurement import *
-from scipy.special import erfc
-import plotly.graph_objects as go
 from numpy.polynomial.hermite import hermgauss
 import matplotlib.pyplot as plt
+import math
+from scipy.special import erfc
 from scipy.optimize import brentq, minimize_scalar
+import plotly.graph_objects as go
+from helper_functions.state_measurement import *
 
-def perr_cs(alpha_grid:np.array, sigma:float, num_samples:int):
-    
+def perr_cs(alpha_grid:np.array, sigma:float, num_samples:int) -> np.array:
+    '''
+    Experimental calculation of the homodyne error probability for coherent states
+    '''
     # Calculation of error probability
     #============================================
     p_err = np.zeros((len(alpha_grid)))
@@ -19,10 +20,12 @@ def perr_cs(alpha_grid:np.array, sigma:float, num_samples:int):
     for i in range(len(alpha_grid)):
 
         # Choose phase from Gaussian distribution
-        phis = np.random.normal(0, sigma, size=num_samples) 
+        #============================================
+        phis = np.random.normal(0, sigma, size = num_samples) 
         wrong_sign_counter = 0
 
         for phi in phis:
+
             coherent_sign = np.random.choice([1, -1])
             prog = sf.Program(1)
 
@@ -43,23 +46,31 @@ def perr_cs(alpha_grid:np.array, sigma:float, num_samples:int):
     return p_err
 
 
-def perr_dss(N_grid:np.array, beta_grid:np.array, sigma:float, num_samples:int):
-
+def perr_dss(N_grid:np.array, beta_grid:np.array, sigma:float, num_samples:int) -> np.array:
+    '''
+    Experimental calculation of the homodyne error probability for displaced squeezed states
+    '''
     #Calculation of error probability
     #============================================
     p_err = np.zeros((len(N_grid), len(beta_grid)))
     
-    for i,N in enumerate(N_grid):
+    for i, N in enumerate(N_grid):
+
         alphas = np.sqrt(N*(1-beta_grid)) 
+
         print(f"\rProgress: {i+1}/{len(N_grid)}", end="", flush=True)
         
         for k, beta in enumerate(beta_grid):
+
             wrong_sign_counter = 0
             r_s = math.asinh(np.sqrt(N*beta))
 
             # Choose phase from Gaussian distribution
+            #============================================
             phis = np.random.normal(0, sigma, size=num_samples)
+
             for phi in phis:
+
                 coherent_sign = np.random.choice([1, -1])
                 prog = sf.Program(1)
 
@@ -81,22 +92,11 @@ def perr_dss(N_grid:np.array, beta_grid:np.array, sigma:float, num_samples:int):
     return p_err
 
 
-def theory_point_dss(N:float, beta:float, sigma:float, gauss:tuple):
-        
-        x_gh, w_gh = gauss
-        a = np.sqrt(N*(1-beta))
-        r = np.arcsinh(np.sqrt(N*beta))
-        phi = np.sqrt(2) * sigma * x_gh
-        V = (np.exp(-2*r)*np.cos(phi)**2 + np.exp(2*r)*np.sin(phi)**2)
-
-        arg = np.sqrt(2)*a*np.cos(phi)/np.sqrt(V)
-        integral = np.sum(w_gh * erfc(arg)) / np.sqrt(np.pi)
-
-        return 0.5*integral
-
-
-def theory_point_cs(N:float, sigma:float, gauss:tuple):
-        
+def theory_point_cs(N:float, sigma:float, gauss:tuple) -> float:
+        '''
+        Numerical calculation of the theoretical homodyne error probability for coherent states,
+        works for a single (N, β) points.
+        '''
         x_gh, w_gh = gauss
         a = np.sqrt(N)
         r = 0
@@ -104,16 +104,40 @@ def theory_point_cs(N:float, sigma:float, gauss:tuple):
         V = (np.exp(-2*r)*np.cos(phi)**2 + np.exp(2*r)*np.sin(phi)**2)
 
         arg = np.sqrt(2)*a*np.cos(phi)/np.sqrt(V)
-        integral = np.sum(w_gh * erfc(arg)) / np.sqrt(np.pi)
+        integral = 0.5*np.sum(w_gh * erfc(arg)) / np.sqrt(np.pi)
 
-        return 0.5*integral
+        return integral
 
 
-def plot_homodyne_perr(sigmas, colors_light, colors_dark, cs=False, dss=False, ):
+def theory_point_dss(N:float, beta:float, sigma:float, gauss:tuple) -> float:
+        '''
+        Numerical calculation of the theoretical homodyne error probability for displaced squeezed states,
+        works for a single (N, β) points.
+        '''
+        x_gh, w_gh = gauss
+
+        a = np.sqrt(N*(1-beta))
+        r = np.arcsinh(np.sqrt(N * beta))
+        phi = np.sqrt(2) * sigma * x_gh
+        V = (np.exp(-2*r) * np.cos(phi)**2 + np.exp(2*r) * np.sin(phi)**2)
+
+        arg = np.sqrt(2) * a * np.cos(phi)/np.sqrt(V)
+        integral = 0.5*np.sum(w_gh * erfc(arg)) / np.sqrt(np.pi)
+
+        return integral
+
+
+def plot_homodyne_perr(sigmas:list, colors_light:list, colors_dark:list, cs:str|bool = False, dss:str|bool = False) -> None:
     '''
-    cs and dss can take these values: 'data', 'theory', 'all', False
+    Plots the homodyne error probability for CS and DSS.
+    CS and DSS can only take the values 'data', 'theory', 'all', or False
     '''
-    
+
+    valid_args = {'data', 'theory', 'all', False}
+
+    if cs not in valid_args or dss not in valid_args:
+         raise KeyError("CS and DSS can only take the values 'data', 'theory', 'all', or False")
+
     n_gh = 100
     gauss = hermgauss(n_gh)
 
@@ -124,7 +148,6 @@ def plot_homodyne_perr(sigmas, colors_light, colors_dark, cs=False, dss=False, )
     for i, sigma in enumerate(sigmas):
 
         #-------------------------  Load data  -------------------------
-
         data_cs = np.load(f"data/phase_diff/perr_data_phase_diff_cs_a40_S10000_sigma{sigma}.npz")
 
         alpha_cs = data_cs["alpha_grid"]
@@ -139,21 +162,18 @@ def plot_homodyne_perr(sigmas, colors_light, colors_dark, cs=False, dss=False, )
         perr_surface_cs = np.zeros_like(N_surface_cs)
 
         #-------------------------  Theoretical curve  -------------------------
-
         for k in range(len(N_cs)):
             for l in range(len(beta_cs)):
+
                 perr_surface_cs [k, l] = perr_cs[k]
                 z_surface_cs[k, l] = theory_point_cs(N_cs[k],  sigma_cs, gauss)
                 
         #-------------------------  R^2  -------------------------
-
         ss_res_cs = np.sum((perr_cs - z_surface_cs[:,0])**2)
         ss_tot_cs = np.sum((perr_cs - np.mean(perr_cs))**2)
-
         R2_cs = 1 - ss_res_cs/ss_tot_cs
 
         #-------------------------  Plot  -------------------------
-
         if cs in ['theory', 'all']:
         
             fig.add_trace(go.Surface(x=N_surface_cs, y=beta_surface_cs, z=z_surface_cs, surfacecolor=np.zeros_like(z_surface_cs), 
@@ -170,7 +190,6 @@ def plot_homodyne_perr(sigmas, colors_light, colors_dark, cs=False, dss=False, )
     for i, sigma in enumerate(sigmas):
 
         #-------------------------  Load data  -------------------------
-
         data_dss = np.load(f"data/phase_diff/perr_data_phase_diff_dss_N40_b40_S10000_sigma{sigma}.npz")
         N_dss =  data_dss["N"]
         beta_dss =  data_dss["beta"]
@@ -181,21 +200,17 @@ def plot_homodyne_perr(sigmas, colors_light, colors_dark, cs=False, dss=False, )
         z_surface_dss = np.zeros_like(N_surface_dss)
 
         #-------------------------  Theoretical curve  -------------------------
-
         for k in range(len(N_dss)):
             for l in range(len(beta_dss)):
 
                 z_surface_dss[k, l] = theory_point_dss(N_dss[k], beta_dss[l], sigma_dss, gauss)
 
         #-------------------------  R^2  -------------------------
-
         ss_res_dss = np.sum((perr_dss - z_surface_dss)**2)
         ss_tot_dss = np.sum((perr_dss - np.mean(perr_dss))**2)
-
         R2_dss = 1 - ss_res_dss/ss_tot_dss
 
         #-------------------------  Plot  -------------------------
-
         if dss in ['theory', 'all']:
                 fig.add_trace(go.Surface( x=N_surface_dss, y=beta_surface_dss, z=z_surface_dss, surfacecolor=np.zeros_like(z_surface_dss), 
                                 colorscale=[[0.0, colors_dark[i]], [1.0, colors_dark[i]]], showscale=False))
@@ -211,10 +226,11 @@ def plot_homodyne_perr(sigmas, colors_light, colors_dark, cs=False, dss=False, )
         fig.show()
 
 
-def beta_threshold_theory(N:float, sigma:float, gauss:tuple):
+def beta_threshold_theory(N:float, sigma:float, gauss:tuple) -> float:
     '''
     Solves the equation F(β_th) = theory_point_dss - theory_point_cs = 0 for a fixed value of N and σ,
     given the fitted parameters of the models. 
+    
     The search for the root is conducted in the interval (β_min, 1) to ensure that the first root for β=0 is not considered,
     where dF(β_min)/dβ = 0.
     '''
@@ -223,12 +239,13 @@ def beta_threshold_theory(N:float, sigma:float, gauss:tuple):
     def F(beta):
         
         point_dss = theory_point_dss(N, beta, sigma, gauss) 
+
         return point_dss - point_cs
     
     # sample to locate minimum
+    #=========================
     beta_grid = np.linspace(0, 1, 500)
     F_b = np.array([F(b) for b in beta_grid])
-
     beta_min = beta_grid[np.argmin(F_b)]
     beta_max = 1
 
@@ -237,19 +254,29 @@ def beta_threshold_theory(N:float, sigma:float, gauss:tuple):
         return beta_upper
     
     except:
-        return 0
+        return 0.0
 
 
-def beta_opt_theory(N, sigma, gauss):
+def beta_opt_theory(N:float, sigma:float, gauss:tuple) -> float:
+    '''
+    Utilized an optimization method to find the value of β in [0,1] which minimizes the homodyne error probability;
+    thus, the optimal value of β.
+    '''
+    def objective(beta:float):
+        '''
+        Defined the objective function for the minimization, which is the homodyne error probability
+        '''
+        return theory_point_dss(N, beta, sigma, gauss)
 
-    def objective(beta):
-        return theory_point_dss( N, beta, sigma, gauss)
-
-    res = minimize_scalar( objective, bounds=(0,1), method="bounded")
+    res = minimize_scalar(objective, bounds=(0,1), method="bounded")
     return res.x
 
 
-def optimal_squeezing(sigmas, colors_opt, colors_th, opt = False, th = False) -> dict:
+def optimal_squeezing(sigmas:list, colors_opt:list, colors_th:list, opt:bool = False, th:bool = True) -> dict:
+
+    '''
+    Plots the threshold and optimal value of the squeezing fraction β as a function of the average photon number for different sigmas.
+    '''
 
     n_gh = 100
     gauss = hermgauss(n_gh)
@@ -257,8 +284,6 @@ def optimal_squeezing(sigmas, colors_opt, colors_th, opt = False, th = False) ->
     plt.figure(figsize=(15,6), dpi=300)
 
     #---------- FIND THRESHOLD ----------
-
-
     beta_opt_dict = {}
 
     for i,sigma in enumerate(sigmas):
@@ -272,13 +297,13 @@ def optimal_squeezing(sigmas, colors_opt, colors_th, opt = False, th = False) ->
         perr_dss = data_dss["p_err_dss"]
         N_surface, beta_surface = np.meshgrid(N, beta, indexing="ij")
 
-
         beta_cs = np.linspace(0, 1, len(N))
         N_surface_cs, beta_surface_cs = np.meshgrid(N, beta_cs, indexing="ij")
         perr_surface_cs = np.zeros_like(N_surface_cs)
 
         for k in range(len(N)):
             for l in range(len(beta_cs)):
+
                 perr_surface_cs [k, l] = perr_cs[k]
 
         # Find intersection points
@@ -304,14 +329,9 @@ def optimal_squeezing(sigmas, colors_opt, colors_th, opt = False, th = False) ->
 
 
         #-------------------------  R^2  -------------------------
-        
         ss_res_th = np.sum((beta_intersection[mask] - beta_th[mask])**2)
         ss_tot_th = np.sum((beta_intersection[mask] - np.mean(beta_intersection[mask]))**2)
-        
         R2_th = 1 - ss_res_th/ss_tot_th
-
-        print(R2_th)
-
 
         # ------------- OPTIMAL ----------------------
         # Minima along beta for each N
@@ -323,10 +343,10 @@ def optimal_squeezing(sigmas, colors_opt, colors_th, opt = False, th = False) ->
             beta_opt_line[idx] = beta_opt_theory(n, sigma, gauss)
         beta_opt_dict[f'sigma_{sigma}'] = beta_opt
 
-
         if th:
             
-            plt.scatter(N_intersection[mask], beta_intersection[mask], s=30, edgecolors='k', color=colors_th[i], marker='D', zorder=10, label = f'σ={sigma:0.1f}: R2 = {R2_th:0.3f}')
+            plt.scatter(N_intersection[mask], beta_intersection[mask], s=30, edgecolors='k', color=colors_th[i], marker='D', zorder=10, 
+                        label = f'σ={sigma:0.1f}: R2 = {R2_th:0.3f}')
             plt.fill_between(N_intersection[mask], beta_th[mask], 0, alpha=0.8, zorder=0, color=colors_th[i])
             plt.plot(N_intersection, beta_th, color='k', linewidth = 3)
 
@@ -347,3 +367,95 @@ def optimal_squeezing(sigmas, colors_opt, colors_th, opt = False, th = False) ->
         
 
     return beta_opt_dict
+
+
+def optimal_squeezing_noisefree(opt:bool = True, th:bool = True) -> None:
+
+    '''
+    Plots the threshold and optimal value of the squeezing fraction β as a function of the average photon number for the noisefree case.
+    '''
+
+    n_gh = 100
+    gauss = hermgauss(n_gh)
+
+    plt.figure(figsize=(15,6), dpi=300)
+
+    #---------- FIND THRESHOLD ----------
+
+    data_cs = np.load(f"data/phase_diff/perr_data_phase_diff_cs_a40_S10000_sigma0.0.npz")
+    perr_cs =  data_cs["p_err_cs"]
+
+    data_dss = np.load(f"data/phase_diff/perr_data_phase_diff_dss_N40_b40_S10000_sigma0.0.npz")
+    N =  data_dss["N"]
+    beta =  data_dss["beta"]
+    perr_dss = data_dss["p_err_dss"]
+    N_surface, beta_surface = np.meshgrid(N, beta, indexing="ij")
+
+    beta_cs = np.linspace(0, 1, len(N))
+    N_surface_cs, beta_surface_cs = np.meshgrid(N, beta_cs, indexing="ij")
+    perr_surface_cs = np.zeros_like(N_surface_cs)
+
+    for k in range(len(N)):
+        for l in range(len(beta_cs)):
+
+            perr_surface_cs [k, l] = perr_cs[k]
+
+    # Find intersection points
+    difference = perr_surface_cs - perr_dss
+        
+    cs = plt.contour(N_surface_cs, beta_surface_cs, difference, levels=[0], alpha=0)
+    path = cs.get_paths()[0]
+    verts = path.vertices
+
+    N_intersection = verts[:,0]
+    beta_intersection = verts[:,1]
+    N_intersection, beta_intersection = zip(*sorted(zip(N_intersection, beta_intersection)))
+    N_intersection = np.array(N_intersection)
+    beta_intersection = np.array(beta_intersection)
+    mask = (N_intersection > 0.01) & (beta_intersection > 0.01)
+
+        
+    # ------------- THRESHOLD ----------------------
+    beta_th = 4*N_intersection/(4*N_intersection + 1)
+
+
+    #-------------------------  R^2 THRESHOLD  -------------------------
+    ss_res_th = np.sum((beta_intersection[mask] - beta_th[mask])**2)
+    ss_tot_th = np.sum((beta_intersection[mask] - np.mean(beta_intersection[mask]))**2)
+    R2_th = 1 - ss_res_th/ss_tot_th
+
+
+    # ------------- OPTIMAL ----------------------
+    # Minima along beta for each N
+    idx = np.argmin(perr_dss, axis=1)   
+    beta_opt = beta[idx]
+    beta_opt_line =  N/(2*N + 1)
+
+    #-------------------------  R^2 OPTIMAL  -------------------------
+    ss_res_opt = np.sum((beta_opt - beta_opt_line)**2)
+    ss_tot_opt = np.sum((beta_opt - np.mean(beta_opt))**2)
+    R2_opt = 1 - ss_res_opt/ss_tot_opt
+
+    if th:
+            
+        plt.scatter(N_intersection[mask], beta_intersection[mask], s=30, edgecolors='k', color='white', marker='D', zorder=10, 
+                    label = fr'$\beta_{{\rm th}}$: $R^2$ = {R2_th:0.3f}')
+        plt.fill_between(N_intersection[mask], beta_th[mask], 0, alpha=0.8, zorder=0, color='blue')
+        plt.plot(N_intersection, beta_th, color='k', linewidth = 3)
+
+    if opt:
+        plt.scatter(N, beta_opt, color='red', edgecolors='k', s=50, marker='o', zorder=10, 
+                    label = fr'$\beta_{{\rm opt}}$: $R^2$ = {R2_opt:0.3f}')
+        plt.plot(N[1:], beta_opt_line[1:], color='k', linewidth = 3)
+
+        if not th:
+             plt.legend()
+        
+    plt.xlabel(r'$N$ (Average number of photons)')
+    plt.ylabel(r'$\beta$ (Squeezing Fraction)')
+    if th:
+        plt.legend()
+
+    plt.tight_layout()
+    plt.show()
+        
