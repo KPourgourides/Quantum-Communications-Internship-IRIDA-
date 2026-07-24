aa62de02c6e8f9c2c259119e953993b311e5b693
KMitsidi <kipmitsidiles@gmail.com>
2026-07-24 14:22:02 +0300

    made colours argument of the plot function

--- helper_functions/phase_diff.py
+++ helper_functions/phase_diff.py
@@ -109,7 +109,7 @@ def theory_point_cs(N:float, sigma:float, gauss:tuple):
         return 0.5*integral
 
 
-def plot_homodyne_perr(sigmas, cs=False, dss=False):
+def plot_homodyne_perr(sigmas, colors_light, colors_dark, cs=False, dss=False, ):
     '''
     cs and dss can take these values: 'data', 'theory', 'all', False
     '''
@@ -118,9 +118,8 @@ def plot_homodyne_perr(sigmas, cs=False, dss=False):
     gauss = hermgauss(n_gh)
 
     fig = go.Figure()
-    colors = [ "#e9f063",  "#d62728",  "#2ca02c",  "#ff7f0e",  "#9467bd",  "#17becf",  "#e377c2",  "#8c564b",  "#bcbd22",  "#7f7f7f",  "#5af74b"]*2
     
-    #------------------  CS  ------------------
+    #-------------------------  CS  -------------------------
 
     for i, sigma in enumerate(sigmas):
 
@@ -144,7 +143,7 @@ def plot_homodyne_perr(sigmas, cs=False, dss=False):
         if cs in ['theory', 'all']:
             # fitted surface
             fig.add_trace(go.Surface(x=N_surface_cs, y=beta_surface_cs, z=z_surface_cs, surfacecolor=np.zeros_like(z_surface_cs), 
-                                colorscale=[[0.0, colors[i]], [1.0, colors[i]]], name="Fit", showscale=False))
+                                colorscale=[[0.0, colors_light[i]], [1.0, colors_light[i]]], name="Fit", showscale=False))
             
         if cs in ['data', 'all']:
                 # Simulation scatter extended in beta
@@ -153,17 +152,12 @@ def plot_homodyne_perr(sigmas, cs=False, dss=False):
                 # No beta dependence
                 for beta in beta_values_cs:
                     fig.add_trace(go.Scatter3d(x=N_cs, y=np.full_like(N_cs, beta), z=perr_cs, mode="markers", 
-                                marker=dict(size=3, color=colors[i]), name=f"Simulation β={beta:.2f}", showlegend=False))
+                                marker=dict(size=3, color=colors_dark[i]), name=f"Simulation β={beta:.2f}", showlegend=False))
 
-    ####################################################################################################
-
-    colors = ["#8E9C09",  "#6F3434",  "#084108",  "#ff7f0e",  "#460880",  "#0c6f7c",  "#5c0642",  "#941c04",  "#8b8b06",  "#212121",  "#092a06",  "#092a06"]*2
-
-    #------------------  DSS  ------------------
+    #-------------------------  DSS  -------------------------
 
     for i, sigma in enumerate(sigmas):
 
-        #===============================
         data_dss = np.load(f"data/phase_diff/perr_data_phase_diff_dss_N40_b40_S10000_sigma{sigma}.npz")
         N_dss =  data_dss["N"]
         beta_dss =  data_dss["beta"]
@@ -178,15 +172,13 @@ def plot_homodyne_perr(sigmas, cs=False, dss=False):
 
                 z_surface_dss[k, l] = theory_point_dss(N_dss[k], beta_dss[l], sigma_dss, gauss)
 
-        # fitted surface
-
         if dss in ['theory', 'all']:
                 fig.add_trace(go.Surface( x=N_surface_dss, y=beta_surface_dss, z=z_surface_dss, surfacecolor=np.zeros_like(z_surface_dss), 
-                                colorscale=[[0.0, colors[i]], [1.0, colors[i]]], name="Fit", showscale=False))
+                                colorscale=[[0.0, colors_dark[i]], [1.0, colors_dark[i]]], name="Fit", showscale=False))
         
         if dss in ['data', 'all']:
                 fig.add_trace(go.Scatter3d(x=N_surface_dss.ravel(), y=beta_surface_dss.ravel(), z=perr_dss.ravel(),
-                mode="markers", marker=dict(size=3, color=colors[i+1]), name=f"σ={sigma}"))
+                mode="markers", marker=dict(size=3, color=colors_light[i]), name=f"σ={sigma}"))
 
         fig.update_layout(scene=dict(xaxis_title="N", yaxis_title=r"β", zaxis = dict(title="P_err", type="log"), 
                             aspectmode ="cube"), width=900, height=750)
