158ffbb1af8f8f52d061bc7dd874de6920008200
KMitsidi <kipmitsidiles@gmail.com>
2026-09-07 20:25:00 +0300

    incorporated squeezing into existing protocol for RR case

--- qkd_sf.py
+++ qkd_sf.py
@@ -8,16 +8,21 @@ from strawberryfields.ops import*
 #                        PROTOCOL
 #=========================================================================================================
 
-def sf_protocol(eta, var, key_len):
+def sf_protocol(eta, var, keylen, V):
 
-    bob_bitstr = []
-    alice_bitstr = []
-    for i in range(key_len):
+    bob_bitstr = np.zeros((keylen))
+    alice_bitstr = np.zeros((keylen))
+    base = np.zeros((keylen))
+
+    r = -np.log(V)/2
+    for i in range(keylen):
         prog = sf.Program(2)
         theta = np.arccos(np.sqrt(eta))
         with prog.context as q:
 
             Vac | q[0]
+            Sgate(r, 0) | q[0] 
+
             alpha_p = np.random.normal(0, np.sqrt(var))
             alpha_q = np.random.normal(0, np.sqrt(var))
             Xgate(alpha_q) | q[0]
@@ -30,25 +35,66 @@ def sf_protocol(eta, var, key_len):
             x = np.random.randint(0, 2)
             if x==0:
                 MeasureHomodyne(0) | q[0]
-                alice_bitstr.append(alpha_q)
+                alice_bitstr[i] = alpha_q
                 
             else:
                 MeasureHomodyne(np.pi/2) | q[0]
-                alice_bitstr.append(alpha_p)
+                alice_bitstr[i] = alpha_p
 
+        base[i] = x
         eng = sf.Engine("gaussian")
         result = eng.run(prog)
-        bob_bitstr.append(result.samples[0][0])
+        bob_bitstr[i]= result.samples[0][0]
         eng.reset()
 
-    return alice_bitstr, bob_bitstr
+    return alice_bitstr, bob_bitstr, base
 
 #=========================================================================================================
 #                        MUTUAL INFORMATION
 #=========================================================================================================
 
-def theoretical_I_AB(var_a, eta):
-    return 1/2*np.log2(1+(var_a*eta))
+def theoretical_I_AB(var_a, eta, V):
+
+    I_AB = 0.5*(1/2*np.log2(1+(var_a*eta)/(1+eta*(V-1))) +  1/2*np.log2(1+(var_a*eta)/(1+eta*(1/V-1))))
+    I_AB_x = 1/2*np.log2(1+(var_a*eta)/(1+eta*(V-1))) 
+    I_AB_p = 1/2*np.log2(1+(var_a*eta)/(1+eta*(1/V-1))) 
+    return I_AB, I_AB_x, I_AB_p
+
+
+def bitstr_to_base(bitstr, quadr, base):
+
+    return [val for i, val in enumerate(bitstr) if base[i] == quadr]
+
+
+def MC_I_AB(var_a_grid, eta, keylen, V):
+
+    size = len(var_a_grid)
+    I_grid = np.zeros((size))
+    I_grid_x = np.zeros((size))
+    I_grid_p = np.zeros((size))
+
+    for i,var_a in enumerate(var_a_grid):
+        alice_bitstr, bob_bitstr, base = sf_protocol(eta, var_a, keylen, V)
+        alice_bitstr_x = bitstr_to_base(alice_bitstr, 0, base)
+        bob_bitstr_x = bitstr_to_base(bob_bitstr, 0, base)
+
+        alice_bitstr_p = bitstr_to_base(alice_bitstr, 1, base)
+        bob_bitstr_p = bitstr_to_base(bob_bitstr, 1, base)
+
+        rho_x = np.corrcoef(alice_bitstr_x, bob_bitstr_x)[0, 1]
+        I_AB_x = -0.5 * np.log2(1 - rho_x**2)
+
+        rho_p = np.corrcoef(alice_bitstr_p, bob_bitstr_p)[0, 1]
+        I_AB_p = -0.5 * np.log2(1 - rho_p**2)
+        
+        I_grid[i] = 0.5*(I_AB_x+ I_AB_p)
+        I_grid_x[i] = I_AB_x
+        I_grid_p[i] = I_AB_p
+
+        print(f"\rProgress: {i+1}/{len(var_a_grid)}", end="", flush=True)
+
+    return I_grid, I_grid_x, I_grid_p
+    
 
 #=========================================================================================================
 #                        KEY RATE
@@ -58,41 +104,75 @@ def key_rate(b, I, x):
     return b*np.array(I)-np.array(x)
 
 #=========================================================================================================
-#                        HOLEVO BOUND
+#                        HOLEVO BOUND RR PROTOCOL
 #=========================================================================================================
 
-def theoretical_holevo_bound(lambda1, lambda2):
-    def G(x):
+def holevo_RR(var_a, eta, V):
 
-        if x==0:
-            return 0
-        return  (x + 1)*np.log2(x + 1) - x*np.log2(x)
-    return G((lambda1 - 1)/2) - G((lambda2 - 1)/2)
+    l1, l2, l3x, l3p = lambda_RR(var_a, eta, V)
+
+    S_AB = G(l1) + G(l2)
+
+    S_A_given_Bx = G(l3x)
+    S_A_given_Bp = G(l3p)
+    S_cond = 0.5*(S_A_given_Bx + S_A_given_Bp)
+
+    chi = S_AB - S_cond
+    chi_x = S_AB - S_A_given_Bx
+    chi_p = S_AB - S_A_given_Bp
+
+    return chi, chi_x, chi_p
 
 
-def lambda_RR(var_a, eta):
+def G(x):
+    # nu is a symplectic eigenvalue, vacuum = 1
+    if x <= 1 + 1e-12:
+        return 0.0
 
-    #lambda1 = 1+(1-eta)*var_a
-    #lambda2 = np.sqrt((1+(1-eta)*var_a)*(1+(1-eta)*var_a/(1+eta*var_a)))
+    n = (x - 1) / 2
 
-    V1 = var_a+1+np.sqrt(2*var_a+var_a**2)
-    V2 = var_a+1-np.sqrt(2*var_a+var_a**2)
+    return (n + 1)*np.log2(n + 1) - n*np.log2(n)
 
-    VB_p = 1/(2*V1)+1/(2*V2)
-    VB_x = (V1+V2)/2
-    CBE_x = np.sqrt(eta*(1-eta))*(1-VB_x)
 
-    lambda1 = np.sqrt(VB_x*(1-eta)+eta)*np.sqrt(VB_p*(1-eta)+eta)
-    lambda2 =  np.sqrt((VB_x*(1-eta)+eta-CBE_x**2/(VB_x*eta+1-eta))*(VB_p*(1-eta)+eta))
+def lambda_RR(var_a, eta, V):
 
-    return lambda1, lambda2
+    # Covariance of Alice's modulated transmitted state
+    sx = V + var_a
+    sp = 1/V + var_a
 
-def lambda_DR(var_a, eta):
-    VE = 1 + (1 - eta) * var_a
+    # Symplectic eigenvalue of the modulated state
+    mu = np.sqrt(sx * sp)
 
-    lambda1 = VE
-    lambda2 = np.sqrt(VE)
+    # Purification
+    cx = np.sqrt((sx/mu) * (mu**2 - 1))
+    cp = np.sqrt((sp/mu) * (mu**2 - 1))
 
-    return lambda1, lambda2
+    # Bob after pure-loss channel
+    bx = eta*sx + (1 - eta)
+    bp = eta*sp + (1 - eta)
+
+    
+    # AB covariance matrix
+    detA = mu**2
+    detB = bx * bp
+    detC = -eta * cx * cp
+
+    Delta = detA + detB + 2*detC
+
+    detAB = ((mu*bx - eta*cx**2)*(mu*bp - eta*cp**2))
+
+    discriminant = Delta**2 - 4*detAB
+    
+    lambda1 = np.sqrt((Delta + np.sqrt(discriminant))/2)
+    lambda2 = np.sqrt((Delta - np.sqrt(discriminant))/2)
+    lambda3_x = np.sqrt(mu * (mu - eta*cx**2/bx))
+    lambda3_p = np.sqrt(mu * (mu - eta*cp**2/bp))
+
+    return lambda1, lambda2, lambda3_x, lambda3_p
+
+
+
+
+
 
 
